95be61c6f3d662023348b5168e9f3a487186a7ef
siuu32 <nosi98758@gmail.com>
2025-02-19 16:13:06 -0400

    solution delete_user

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php include '../includes/header.php'; ?>
 
 <div class="container mx-auto text-center mt-10">
-    <h1 class="text-4xl font-bold text-teal-400">Bienvenido al Sistema de Gestión de Usuarios</h1>
+    <h1 class="text-4xl ">Bienvenido al Sistema de Gestión de Usuarios</h1>
     <p class="text-lg mt-4">Regístrate o inicia sesión para acceder a tu cuenta.</p>
 </div>
 
